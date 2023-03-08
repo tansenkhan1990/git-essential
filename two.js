118aff28faa31b09e2f9bfeaafc2e82508d23715
tansenkhan1990 <tansenkhan1990@gmail.com>
2023-03-08 15:31:20 +0100

    add chenges for merge testing

--- two.js
+++ two.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const one = () => {
   return (
-    <div>for sub-two for merge testing</div>
+    <div>for sub-two for merge testing for new merge testing</div>
   )
 }
 
