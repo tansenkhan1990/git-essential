2f3b7eccf4a52edf1f0736f15a7b5deab7ec4b55
tansenkhan1990 <tansenkhan1990@gmail.com>
2023-03-08 16:39:24 +0100

    add previous code

--- three.js
+++ three.js
@@ -2,10 +2,6 @@ import React from 'react'
 
 const three = () => {
   return (
-<<<<<<< HEAD
-    <div>this is create after git two</div>
-=======
->>>>>>> 9966f36 (add new file)
     <div>three</div>
   )
 }
