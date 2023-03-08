63d28d79a86a5fcbfbc194c30067da82ea57174f
tansenkhan1990 <tansenkhan1990@gmail.com>
2023-03-08 15:36:42 +0100

    add chenges for merge testing

--- two.js
+++ two.js
@@ -2,11 +2,7 @@ import React from 'react'
 
 const one = () => {
   return (
-<<<<<<< HEAD
-    <div>for sub-two for merge testing pokeo</div>
-=======
-    <div>for sub-two for merge testing for new merge testing</div>
->>>>>>> main
+    <div>for sub-two</div>
   )
 }
 
