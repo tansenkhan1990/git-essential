4f3f284fc1af206058c9b409952fd80c48fb8280
tansenkhan1990 <tansenkhan1990@gmail.com>
2023-03-14 22:58:33 +0100

    add new cherry-pick

--- one.js
+++ one.js
@@ -2,8 +2,7 @@ import React from 'react'
 
 const one = () => {
   return (
-    <div>this is updated version sub-one</div>
-    <div>testing cherry-pick</div>
+    <div>this is for cherry pick</div>
   )
 }
 
